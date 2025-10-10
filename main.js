011f3c16184bcabe94f2668b140841c3e83ec46b
Melissa Lin <melissalin128@gmail.com>
2025-10-10 09:28:23 +0200

    Download Resume Button

--- main.js
+++ main.js
@@ -111,3 +111,17 @@ if (contactForm) {
     setTimeout(typeChar, 800);
   });
 })();
+
+
+
+
+// motion transition fade in
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.fade-in').forEach((el) => observer.observe(el));
